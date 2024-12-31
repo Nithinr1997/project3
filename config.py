dd0c484ddceec75b4b237659ca8dd7f571b86386
Nithinr1997 <46882949+Nithinr1997@users.noreply.github.com>
2024-12-31 10:52:01 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#Nithinr1997/another_one
+
+# Number of rows and columns
+num_rows, num_columns = sales_data.shape
+print(f"Number of rows: {num_rows}")
+print(f"Number of columns: {num_columns}")
+
+# Types of variables
+print("\nColumn Data Types:")
+print(sales_data.dtypes)
