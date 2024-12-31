ede43383e4403838502288aa3da717e1d8ac4d56
Nithin R <nithin.rajendra97@gmail.com>
2024-12-31 10:57:25 +0530

    Initial commit for project 3

---
+++ Service/Unit_test.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    '''
+    This is a test:
+    >>> add(2, 2)
+    5
+    '''
+    return a + b
---
+++ Service_2/unit_test_2.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestNotebook(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(2, 2), 5)
+
+
+unittest.main(argv=[''], verbosity=2, exit=False)
